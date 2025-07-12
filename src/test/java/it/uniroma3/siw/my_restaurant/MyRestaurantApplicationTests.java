550279f45711e1f25a8c9d83334589e0a891e7fa
fabri7196 <fabry_c96@hotmail.it>
2025-07-12 16:15:43 +0200

    First Commit

---
+++ src/test/java/it/uniroma3/siw/my_restaurant/MyRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.my_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
